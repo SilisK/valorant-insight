72b2a6bbf10ecb2765167778aa6d6b4ca0bcaeea
SilisK <kleemoffdeveloper@gmail.com>
2023-11-12 06:15:51 -0500

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Valorant Insight</h1>
+<p>
+    A front-end application that extends the functionality of <a href="https://valorant-api.com/">Valorant API</a>.
+</p>
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="app grid min-h-screen place-items-center">
+      <div className="grid gap-5 text-center">
+        <h1 className="text-5xl font-bold">Valorant Insight</h1>
+        <p>Work in progress...</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
