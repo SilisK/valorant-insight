88560ae1f64aa3a80dc3ef15cb05bc27cc10b559
SilisK <kleemoffdeveloper@gmail.com>
2023-11-13 03:33:09 -0500

    testing mobile weapon icons

--- src/components/WeaponIcon.jsx
+++ src/components/WeaponIcon.jsx
@@ -1,6 +1,6 @@
 export default function WeaponIcon({ weapon }) {
   return (
-    <div className="weapon-icon bg-white p-2 rounded-3xl shadow select-none w-max cursor-pointer grid gap-2 place-items-center">
+    <div className="weapon-icon bg-white p-2 rounded-3xl shadow select-none cursor-pointer grid gap-2 place-items-center">
       <div className="weapon-image">
         <img src={weapon.displayIcon} />
       </div>
