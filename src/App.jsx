d38ee11f7498f86af92c3c60f338d59132082733
SilisK <kleemoffdeveloper@gmail.com>
2023-11-13 03:30:07 -0500

    updated responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -40,9 +40,9 @@ function App() {
         className="grid gap-20 pb-20 place-items-center w-full min-h-screen
        items-start"
       >
-        <div className="px-5 grid gap-5 py-5 w-full">
+        <div className="px-2 sm:px-5 grid gap-5 py-5 w-full">
           <h1 className="text-6xl sm:text-8xl font-bold">Agents</h1>
-          <div className="grid gap-10 grid-flow-col place-items-center overflow-x-scroll bg-white shadow-md rounded-xl p-8">
+          <div className="grid gap-10 grid-flow-col place-items-center overflow-x-scroll sm:bg-white sm:shadow-md sm:ounded-xl px-1 py-3 sm:p-8">
             {agents ? (
               agents.map((agent, i) => <AgentIcon agent={agent} key={i} />)
             ) : (
@@ -54,9 +54,9 @@ function App() {
           </div>
         </div>
         {/*  */}
-        <div className="px-5 grid gap-5 py-5 w-full">
+        <div className="px-2 sm:px-5 grid gap-5 py-5 w-full">
           <h1 className="text-6xl sm:text-8xl font-bold">Weapons</h1>
-          <div className="grid gap-10 grid-flow-col place-items-center overflow-x-scroll bg-white shadow-md rounded-xl p-8">
+          <div className="grid gap-10 grid-flow-col place-items-center overflow-x-scroll sm:bg-white sm:shadow-md sm:rounded-xl px-1 py-3 sm:p-8">
             {weapons ? (
               weapons.map((weapon, i) => <WeaponIcon weapon={weapon} key={i} />)
             ) : (
@@ -68,9 +68,9 @@ function App() {
           </div>
         </div>
         {/*  */}
-        <div className="px-5 grid gap-5 py-5 w-full">
+        <div className="px-2 sm:px-5 grid gap-5 py-5 w-full">
           <h1 className="text-6xl sm:text-8xl font-bold">Maps</h1>
-          <div className="grid gap-10 sm:grid-flow-col place-items-center sm:overflow-x-scroll bg-white shadow-md rounded-xl p-8">
+          <div className="grid gap-10 sm:grid-flow-col place-items-center sm:overflow-x-scroll sm:bg-white sm:shadow-md sm:rounded-xl px-1 py-3 sm:p-8">
             {maps ? (
               maps.map((map, i) => <MapIcon map={map} key={i} />)
             ) : (
