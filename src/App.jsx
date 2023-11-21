63ae0a4b66371c6cdb07d024b7e040ceb3320cb1
SilisK <kleemoffdeveloper@gmail.com>
2023-11-21 07:31:13 -0500

    re-designing for vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { useEffect, useState } from "react";
-import Footer from "./components/Footer";
-import AgentIcon from "./components/AgentIcon";
-import Navbar from "./components/Navbar";
-import WeaponIcon from "./components/WeaponIcon";
-import MapIcon from "./components/MapIcon";
-import InfoPage from "./components/InfoPage";
 
 function App() {
   const [agents, setAgents] = useState();
   const [weapons, setWeapons] = useState();
   const [maps, setMaps] = useState();
-
-  const [info, setInfo] = useState();
-  const [page, setPage] = useState("");
 
   useEffect(() => {
     fetch("https://valorant-api.com/v1/agents")
@@ -37,89 +28,9 @@ function App() {
       .catch((error) => {});
   }, []);
 
-  useEffect(() => {
-    window.scroll(0, 0);
-  }, [page]);
-
-  return (
-    <div className="app min-h-screen grid">
-      <Navbar setPage={setPage} />
-      <main
-        className="grid gap-20 pb-20 place-items-center min-h-screen
-       items-start"
-      >
-        {page === "Info" ? (
-          <InfoPage info={info} setPage={setPage} />
-        ) : (
-          <>
-            <div className="grid gap-5 py-5 w-full">
-              <h1 className="px-5 text-6xl sm:text-8xl font-bold">Agents</h1>
-              <div className="grid gap-10 grid-flow-col place-items-center overflow-x-scroll px-1 py-3 sm:p-8">
-                {agents ? (
-                  agents.map((agent, i) => (
-                    <AgentIcon
-                      agent={agent}
-                      key={i}
-                      setPage={setPage}
-                      setInfo={setInfo}
-                    />
-                  ))
-                ) : (
-                  <img
-                    src="https://www.svgrepo.com/show/448500/loading.svg"
-                    className="gizmo"
-                  />
-                )}
-              </div>
-            </div>
-            {/*  */}
-            <div className="grid gap-5 py-5 w-full">
-              <h1 className="px-5 text-6xl sm:text-8xl font-bold">Weapons</h1>
-              <div className="grid gap-10 grid-flow-col place-items-center overflow-x-scroll px-1 py-3 sm:p-8">
-                {weapons ? (
-                  weapons.map((weapon, i) => (
-                    <WeaponIcon
-                      weapon={weapon}
-                      key={i}
-                      setPage={setPage}
-                      setInfo={setInfo}
-                    />
-                  ))
-                ) : (
-                  <img
-                    src="https://www.svgrepo.com/show/448500/loading.svg"
-                    className="gizmo"
-                  />
-                )}
-              </div>
-            </div>
-            {/*  */}
-            <div className="grid gap-5 py-5 w-full">
-              <h1 className="px-5 text-6xl sm:text-8xl font-bold">Maps</h1>
-              <div className="grid gap-10 sm:grid-flow-col place-items-center sm:overflow-x-scroll px-1 py-3 sm:p-8">
-                {maps ? (
-                  maps.map((map, i) => (
-                    <MapIcon
-                      map={map}
-                      key={i}
-                      setPage={setPage}
-                      setInfo={setInfo}
-                    />
-                  ))
-                ) : (
-                  <img
-                    src="https://www.svgrepo.com/show/448500/loading.svg"
-                    className="gizmo"
-                  />
-                )}
-              </div>
-            </div>
-          </>
-        )}
-      </main>
-      <Footer />
-    </div>
-  );
+  return <div className="app min-h-screen grid place-items-center">
+    <h1 className="text-5xl font-semibold">Hello, Vercel.</h1>
+  </div>;
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,3 @@
-export default function Navbar({ setPage }) {
-  return (
-    <nav className="text-center select-none bg-white py-3 px-3 sm:px-10 border-b-4 my-5 grid gap-3 sm:grid-flow-col grid-col-3 items-center justify-between">
-      <a
-        onClick={() => {
-          setPage("");
-        }}
-      >
-        Home
-      </a>
-      <a href="https://valorant-api.com/" target="_blank">
-        API Source
-      </a>
-      <a>
-        About
-      </a>
-    </nav>
-  );
+export default function Navbar() {
+  return <nav className=""></nav>;
 }
